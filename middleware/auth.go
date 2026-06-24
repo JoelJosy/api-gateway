ab1b2cf6e25b703e641a4859798126e4302da9bc
Joel Josy <joeljosy79@gmail.com>
2026-06-24 13:44:10 +0400

    feat(middleware): setup jwt verification and validation middleware

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,14 +1,27 @@
 package middleware
 
 import (
+	"context"
+	"crypto/rsa"
+	"fmt"
 	"net/http"
 	"strings"
 
 	"github.com/JoelJosy/api-gateway/config"
+	"github.com/golang-jwt/jwt/v5"
 )
 
+// payload / data expected in jwt
+type GatewayClaims struct {
+	UserID string `json:"user_id"`
+    Email  string `json:"email"`
+	jwt.RegisteredClaims
+}
+
+var claimsKey = contextKey{}
+
 // AuthMiddleware takes your gateway configurations as dependencies
-func AuthMiddleware(cfg *config.Config) Middleware {
+func AuthMiddleware(cfg *config.Config, verifyKey *rsa.PublicKey) Middleware {
 	// return middleware
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -36,11 +49,49 @@ func AuthMiddleware(cfg *config.Config) Middleware {
 
 			// Auth required
 			authHeader := r.Header.Get("Authorization")
+			// 401 unauthorized
 			if (len(authHeader) == 0 || strings.HasPrefix(authHeader, "Bearer ")) {
 				http.Error(w, "Unauthorized", http.StatusUnauthorized)
 				return 
 			}
+
+			tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+			// Provide pointer to empty struct where decoded claims will be stored
+			// Callback provides key used to verify signature
+			// JWT library: parses header/payload, gets key via callback,
+			// verifies signature, then decodes payload into struct
+			token, err := jwt.ParseWithClaims(tokenString, &GatewayClaims{}, func(token *jwt.Token) (any, error) {
+				// Verify the signing method is RS256
+				if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+					return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+				}
+				return verifyKey, nil
+			})
+
+			// Example: 
+			// token := &jwt.Token{
+			//     Claims: &GatewayClaims{
+			//         UserID: "123",
+			//         Role: "admin",
+			//     },
+			//     Valid: true
+			// }
+
+			// reject if parsing failed, signature invalid, or token expired/invalid
+			if err != nil || !token.Valid {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return 
+			}
 			
+			// convert type from interface{} to *GatewayClaims and save in claims var
+			if claims, ok := token.Claims.(*GatewayClaims); ok {
+				// inject user id from payload into
+				ctx := context.WithValue(r.Context(), claimsKey, claims.UserID)
+				newRequest := r.WithContext(ctx)
+
+				next.ServeHTTP(w, newRequest)
+				return
+			}
 
 			next.ServeHTTP(w, r)
 		})
