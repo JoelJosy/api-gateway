aa8ba4803aedf02d5301600541c9da8a9114104d
Joel Josy <joeljosy79@gmail.com>
2026-06-24 14:28:09 +0400

    feat: add auth to middleware chain + testing

--- main.go
+++ main.go
@@ -17,13 +17,18 @@ func main() {
 		log.Fatalf("failed to load config: %v", err)
 	}
 
+	pubKey, err := config.ParsePublicKeyPEM("./public.pem")
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
 	fmt.Printf("API Gateway starting on port %d\n", cfg.Port)
 
 	// init proxy
 	r := router.NewRouter(cfg.Routes)
 	p := proxy.NewProxy(r)
 
-	handler := middleware.Chain(p, middleware.LoggerMiddleware)
+	handler := middleware.Chain(p, middleware.LoggerMiddleware, middleware.AuthMiddleware(cfg, pubKey))
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), handler))
 }
